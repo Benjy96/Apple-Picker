407f0b2ad569641f4c9aa0b12e7eac5808af4d61
Benjy96 <benjamindickey@hotmail.co.uk>
2018-06-05 11:23:38 +0100

    Add smooth movement to android/pc
    Speed variable needs to be higher for android, lowered for pc

--- Apple Plicker/Assets/Scripts/Basket.cs
+++ Apple Plicker/Assets/Scripts/Basket.cs
@@ -5,18 +5,17 @@ using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
 
+    public float speed = 30f;
+
     AudioSource audio;
     
     private void Start()
     {
         audio = gameObject.GetComponent<AudioSource>();
-
     }
 
     void Update ()
     {
-        
-
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 
         // Get the current screen position of the mouse from Input
@@ -28,16 +27,20 @@ public class Basket : MonoBehaviour {
         // Convert the point from 2D screen space into 3D game world space
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); 
 
-        //Move the x position of this Basket to the x position of the Mouse
+        // Move the x position of this Basket to the x position of the Mouse
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
-        this.transform.position = pos;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * speed);
 
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-        transform.Translate(Input.acceleration.x, 0, 0);
-        //have the basket stop one it reaches edge of screen
+        bool move = false;
+        if (Input.acceleration.x > 0 || Input.acceleration.x < 0) move = true;
 
-        #endif
+        if (move) transform.position = Vector2.Lerp(transform.position,
+             new Vector2(transform.position.x + Input.acceleration.x, transform.position.y),
+             Time.deltaTime * speed);
+        //transform.Translate(Input.acceleration.x, 0f, 0f);
+#endif
     }
 
     void OnCollisionEnter2D(Collision2D coll)
